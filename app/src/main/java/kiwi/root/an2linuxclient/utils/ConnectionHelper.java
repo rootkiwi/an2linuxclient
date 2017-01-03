99ac146ef8a79a87146349f4ffeb00a1e752323e
rootkiwi <rootkiwi@users.noreply.github.com>
2017-01-03 19:02:27 +0100

    Add IPv6 regex for user input validation

--- app/src/main/java/kiwi/root/an2linuxclient/utils/ConnectionHelper.java
+++ app/src/main/java/kiwi/root/an2linuxclient/utils/ConnectionHelper.java
@@ -54,12 +54,16 @@ public class ConnectionHelper {
         return false;
     }
 
-    public static boolean checkIfValidIpOrHostname(String ipOrHostname) {
+    private static boolean checkIfValidIpOrHostname(String ipOrHostname) {
         // source: http://stackoverflow.com/a/3824105
         String validHostnameRegex = "^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])(\\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9]))*$";
         // source: http://stackoverflow.com/a/106223
-        String validIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
-        return ipOrHostname.matches(validHostnameRegex) && ipOrHostname.length() <= 255 || ipOrHostname.matches(validIpAddressRegex);
+        String validIPv4Regex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+        // source: http://stackoverflow.com/a/17871737
+        String validIPv6Regex = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))";
+        return ipOrHostname.matches(validHostnameRegex) && ipOrHostname.length() <= 255
+                || ipOrHostname.matches(validIPv4Regex)
+                || ipOrHostname.matches(validIPv6Regex);
     }
 
     public static boolean checkIfValidAddressAndPortInput(EditText ipOrHostnameEditText,
