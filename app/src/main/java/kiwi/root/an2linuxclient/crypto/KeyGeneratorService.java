23e31510cdfca90d925e6ff58aa520b60011b3f3
rootkiwi <rootkiwi@users.noreply.github.com>
2017-01-21 17:36:45 +0100

    Add missing license notices

--- app/src/main/java/kiwi/root/an2linuxclient/crypto/KeyGeneratorService.java
+++ app/src/main/java/kiwi/root/an2linuxclient/crypto/KeyGeneratorService.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2017 rootkiwi
+ *
+ * AN2Linux-client is licensed under GNU General Public License 3.
+ *
+ * See LICENSE for more details.
+ */
+
 package kiwi.root.an2linuxclient.crypto;
 
 import android.app.IntentService;
